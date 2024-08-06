1b4bfcf1b81d7cdf229258106bf231463c906361
PAMELA OLIVEIRA SANTOS <pamela.osantos3@senacsp.edu.br>
2024-08-05 21:58:41 -0300

    first commit

--- PHP/Modelo/Main.php
+++ PHP/Modelo/Main.php
@@ -1,28 +1,104 @@
 <?php
-    namespace PHP\Modelo; //Definir o local do projeto para se conversarem
-    //require once ele executa uma vez a leitura e armazena
-    //require - leitura varias vezes
+    namespace PHP\Modelo; //Definir o local do projeto
     require_once('Pessoa.php');
     require_once('Funcionario.php');
     require_once('Cliente.php');
-
-    //pessoa - chamar objeto
-    //funcionário - chamar objeto
-
-    $pessoa1 = new Pessoa("125.254.545-55","Pâmela","119999999","Rua O, 174");    
-    $pessoa2 = new Pessoa("125.254.000-55","Aroldo","119999999","Rua 1, 174");
-   
-    $funcionario1 = new Funcionario(1188, "Pâmela",1000,"Atendente","Vergueiro, 55- SBC", "11999555588");
-
-    $cliente1 = new Cliente("133.254.545-55","Talita","119999999","Rua O, 174","24/07/1983", 1000);
-
-    echo $pessoa1->imprimir();
-    echo $pessoa2->imprimir();
-    echo $funcionario1->imprimir();
-    echo $cliente1->imprimir();
-    
-
+    require_once('Endereco.php');
+    require_once('DAO/Conexao.php');
+    require_once('DAO/Inserir.php');
+    require_once('DAO/Consultar.php');
+    require_once('DAO/Atualizar.php');
+    require_once('DAO/Excluir.php');
  
-    
-
+    use PHP\Modelo\DAO\Conexao;//Direcionar o arquivo
+    use PHP\Modelo\DAO\Inserir;
+    use PHP\Modelo\DAO\Consultar;
+    use PHP\Modelo\DAO\Atualizar;
+    use PHP\Modelo\DAO\Excluir;
+ 
+    $conexao = new Conexao();
+    $inserir = new Inserir();
+    $consultar = new Consultar();
+    $atualizar = new Atualizar();
+    $excluir = new Excluir();
+    /*echo $inserir->cadastrarEndereco($conexao,
+                                 1,
+                                'Rua jiparana',
+                                 123,
+                                'Berinni',
+                                'São Paulo',
+                                'São Paulo',
+                                'SP',
+                                'Brasil',
+                                '03586921');*/
+ 
+    /*echo $inserir->cadastrarCliente($conexao,
+                                '12345678910',
+                                'Davi',
+                                '11981230912',
+                                '17/04/1989',
+                                1000,
+                                1);*/
+    /*$consultar->consultarIndividual($conexao,
+                                    'cliente',
+                                    'cpf',
+                                    '12345678910');
+   
+ 
+ 
+    $atualizar->atualizarCliente($conexao,
+                                "nome",
+                                "Claudia",
+                                "12345678910");
+    */
+ 
+    $excluir->excluirCliente($conexao,"12345678910");
+ 
+    $consultar->consultarTudo($conexao,
+                             'cliente');
+ 
+/*
+    $enderecoCliente1 = new Endereco('Rua jiparana',
+                                     123,
+                                    'Berinni',
+                                    'São Paulo',
+                                    'São Paulo',
+                                    'SP',
+                                    'Brasil',
+                                    '03586921');
+ 
+    $cliente1 = new Cliente('12345678910',
+                           'Davi',
+                           '11981230912',
+                            $enderecoCliente1,
+                            '17/04/1989',
+                            1000);
+ 
+    echo $cliente1->imprimir();
+    echo $enderecoCliente1->imprimir();
+ 
+    echo "<br><br>";
+ 
+    $enderecoFunc1 = new Endereco("Rua Jeri",
+                   123,
+                   "Realengo",
+                   "Rio de Janeiro",
+                   "Rio de Janeiro",
+                   "RJ",
+                   "Brasil",
+                   "01312312");
+ 
+    $funcionario1 = new Funcionario("12136598565",
+                                    123,
+                                    "Astolfo",
+                                    1256,
+                                    "Gerente",
+                                    $enderecoFunc1,
+                                    "1190909090");
+   
+    echo $funcionario1->imprimir();
+    echo $enderecoFunc1->imprimir();
+*/
+ 
 ?>
+ 
